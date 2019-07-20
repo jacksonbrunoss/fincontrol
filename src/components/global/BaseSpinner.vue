e6b482c066ba3dde4b9b5bcd8e2d8fde617830ed
jacksonbrunoss <oi.jbss@gmail.com>
2019-07-20 14:25:54 -0300

    Eslinter e comp. BaseSpinner

---
+++ src/components/global/BaseSpinner.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="base-spinner fa-3x">
+    <i class="fas fa-circle-notch fa-spin"></i>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'baseSpinner',
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../assets/scss/variables';
+
+.base-spinner {
+  top: 0;
+  bottom: 0;
+  right: 0;
+  left: 0;
+  z-index: 10000000;
+  position: absolute;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #666;
+  color: $featured;
+}
+</style>
